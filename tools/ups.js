75965e4ece9a21b6f7fa860958a9f07db30041a5
hardgames <95406644+hardgs@users.noreply.github.com>
2022-03-08 11:59:38 -0800

    Add files via upload

---
+++ tools/ups.js
@@ -0,0 +1,15 @@
+function __d_m(){
+    var sm = Array("",
+    "<linK rel='stylesheet' href='tools//me.css'>",
+    "<ul id='me'>",
+    "<li><a href='https:////hardgs.github.io/'>خانه</a></li>",
+    "<li><span> بازی ها </span></li>",
+    "<li><span> دوره ها </span></li>"
+    );
+    for(i=0;i<=sm.length-1;i++){
+        document.write(sm[i]);
+    }
+}
+function menu(){
+    __d_m();
+}
