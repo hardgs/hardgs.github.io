7253a8fe2a7b00e3754974fa60175270252ff1c7
hardgames <95406644+hardgs@users.noreply.github.com>
2022-03-09 11:24:48 -0800

    Add files via upload

--- tools/ups.js
+++ tools/ups.js
@@ -1,11 +1,11 @@
 function __d_m(){
     var sm = Array("",
-    "<linK rel='stylesheet' href='https:////hardgames.github.io//tools//me.css'>",
+    "<linK rel='stylesheet' href='https://hardgs.github.io/tools/me.css'>",
     "<ul id='me'>",
-    "<li><a href='https:////hardgs.github.io/' id='sm'>خانه</a></li>",
+    "<li><a href='https://hardgs.github.io/' id='sm'>خانه</a></li>",
     "<li><span> بازی ها </span></li>",
     "<li><span> دوره ها </span>",
-    "<a href='https:////hardgs.github.io//topic//0hd.html'> C/C++ </a></li>",
+    "<a href='https://hardgs.github.io/topic/0hd.html'> C/C++ </a></li>",
     "<li><span> بازی ها ی هک شده </span></li>",
     "<li><span> توضیح </span></li>",
     "</ul>"
